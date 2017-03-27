4cf58b401d7131d577b40ea6b35150a38800371e
Chandramowli J <chamowstudios@outlook.com>
2017-03-27 23:24:43 +0530

    Add logout and toast notification plugin

--- home/views.py
+++ home/views.py
@@ -38,12 +38,12 @@ class UserFormView(View):
             user.set_password(password)
             user.save()
 
-            user = authenticate(username='username', password='password')
+            user = authenticate(username=username, password=password)
 
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return redirect('index')
+                    return render(request, 'home/index.html' )
 
         return render(request, self.template_name, {'form': form})
 
