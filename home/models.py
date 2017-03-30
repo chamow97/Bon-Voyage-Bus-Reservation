b3b72606af5b436ac673c09f6cdef6944eeae044
Chandramowli J <chamowstudios@outlook.com>
2017-03-31 02:37:42 +0530

    Add Bus models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 from django.http import HttpResponse
+from django.contrib.auth.models import Permission, User
+
+class Bus(models.Model):
+    name = models.CharField(max_length=300)
+    source = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    capacity = models.IntegerField()
+    fare = models.FloatField()
+
+
 
--- home/static/home/maps.js
+++ home/static/home/maps.js
@@ -2,7 +2,7 @@ function initMap() {
     
     var location = {lat: 12.997173, lng: 77.584762};
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
+        zoom: 10,
         center: location
     });
     var marker = new google.maps.Marker({
