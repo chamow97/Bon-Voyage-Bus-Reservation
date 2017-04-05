d350b961d43189e8ff6ea7a5df7ad23836de7ac6
Chandramowli J <chamowstudios@outlook.com>
2017-04-05 11:12:42 +0530

    Fix bugs!

--- home/static/home/home.js
+++ home/static/home/home.js
@@ -1,4 +1,4 @@
 $(window).scroll( function() {
     var x = parseInt($("#logo").css("margin-right"));
-    $("#logo").css("margin-right", function(i) { return i + ($(window).scrollTop() * 3)});
+    $("#logo").css("margin-right", function(i) { return i + ($(window).scrollTop() * 2)});
 });	
