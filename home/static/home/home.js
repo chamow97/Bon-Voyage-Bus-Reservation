d34ad22714df3f1eb3e72351aa796b38e1ad9a73
chamow <chamowstudios@outlook.com>
2017-04-16 08:20:36 +0530

    Clean the code

--- home/static/home/home.js
+++
@@ -1,4 +0,0 @@
-$(window).scroll( function() {
-    var x = parseInt($("#logo").css("margin-right"));
-    $("#logo").css("margin-right", function(i) { return i + ($(window).scrollTop() * 2)});
-});	
